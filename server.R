79b624a74258f8ff8e9db080290faea72f45c44a
mattsedlar <sedlarm@gmail.com>
2015-09-17 15:04:09 -0700

    Working on probability function

--- server.R
+++ server.R
@@ -6,18 +6,22 @@ shinyServer(function(input,output){
 
   output$prob <- renderText({
     state.df <- subset(tidydata, state == input$state)
-    age <-as.numeric(input$age)
-    income <- input$income
+    age <- as.numeric(input$age)
+    income <- as.numeric(input$income)
     
     # method of commute by age
     totaldrovealone <- state.df$drovealone16yearsandover[1] * state.df[,age]
     totalcarpool <- state.df$carpooled16yearsandover[1] * state.df[,age+7]
     totalpublictrans <- state.df$publictrans16yearsandover[1] * state.df[,age+14]
-
+    
+    # sum of above methods
     sum <- sum(totaldrovealone,totalcarpool,totalpublictrans)
     
-    probA <- sum((1*(totaldrovealone/sum)) + (2 *(totalcarpool/sum)) + (3 * (totalpublictrans/sum)))
-    probA    
+    # method of commute by income
+    totalincomelevel.drivealone <- state.df$drovealone16yearsandoverwithearnings * state.df[,income]
+    totalincomelevel.carpool <- state.df$drovealone16yearsandoverwithearnings * state.df[,income + 9]
+    totalincomelevel.publictrans <- state.df$drovealone16yearsandoverwithearnings * state.df[,income + 18]
+    
   })
 
 })
