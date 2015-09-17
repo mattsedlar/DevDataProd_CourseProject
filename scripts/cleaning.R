8890520662637a2e65719ab3086a4259634367b0
mattsedlar <sedlarm@gmail.com>
2015-09-17 16:49:42 -0700

    Updated to focus on income only

--- scripts/cleaning.R
+++ scripts/cleaning.R
@@ -16,27 +16,6 @@ tidydata <- data
 
 # selecting only columns necessary for calculations
 tidydata <- tidydata %>% select(Geography,
-                                Car..truck..or.van....drove.alone..Estimate..Workers.16.years.and.over,
-                                Car..truck..or.van....drove.alone..Estimate..AGE...16.to.19.years,
-                                Car..truck..or.van....drove.alone..Estimate..AGE...20.to.24.years,
-                                Car..truck..or.van....drove.alone..Estimate..AGE...25.to.44.years,
-                                Car..truck..or.van....drove.alone..Estimate..AGE...45.to.54.years,
-                                Car..truck..or.van....drove.alone..Estimate..AGE...55.to.59.years,
-                                Car..truck..or.van....drove.alone..Estimate..AGE...60.years.and.over,
-                                Car..truck..or.van....carpooled..Estimate..Workers.16.years.and.over,
-                                Car..truck..or.van....carpooled..Estimate..AGE...16.to.19.years,
-                                Car..truck..or.van....carpooled..Estimate..AGE...20.to.24.years,
-                                Car..truck..or.van....carpooled..Estimate..AGE...25.to.44.years,
-                                Car..truck..or.van....carpooled..Estimate..AGE...45.to.54.years,
-                                Car..truck..or.van....carpooled..Estimate..AGE...55.to.59.years,
-                                Car..truck..or.van....carpooled..Estimate..AGE...60.years.and.over,
-                                Public.transportation..excluding.taxicab...Estimate..Workers.16.years.and.over,
-                                Public.transportation..excluding.taxicab...Estimate..AGE...16.to.19.years,
-                                Public.transportation..excluding.taxicab...Estimate..AGE...20.to.24.years,
-                                Public.transportation..excluding.taxicab...Estimate..AGE...25.to.44.years,
-                                Public.transportation..excluding.taxicab...Estimate..AGE...45.to.54.years,
-                                Public.transportation..excluding.taxicab...Estimate..AGE...55.to.59.years,
-                                Public.transportation..excluding.taxicab...Estimate..AGE...60.years.and.over,
                                 Car..truck..or.van....drove.alone..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings,
                                 Car..truck..or.van....drove.alone..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....1.to..9.999.or.loss,
                                 Car..truck..or.van....drove.alone..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....10.000.to..14.999,
@@ -69,26 +48,17 @@ tidydata <- tidydata %>% select(Geography,
 # state
 colnames(tidydata) <- gsub("Geography","state",colnames(tidydata))
 # drove alone
-colnames(tidydata) <- gsub("Car..truck..or.van....drove.alone..Estimate..Workers.","drovealone",colnames(tidydata))
-colnames(tidydata) <- gsub("Car..truck..or.van....drove.alone..Estimate..AGE...","drovealone",colnames(tidydata))
 colnames(tidydata) <- gsub("Car..truck..or.van....drove.alone..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.","drovealone",colnames(tidydata))
 # carpool
-colnames(tidydata) <- gsub("Car..truck..or.van....carpooled..Estimate..Workers.","carpooled",colnames(tidydata))
-colnames(tidydata) <- gsub("Car..truck..or.van....carpooled..Estimate..AGE...","carpooled",colnames(tidydata))
 colnames(tidydata) <- gsub("Car..truck..or.van....carpooled..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.","carpooled",colnames(tidydata))
 # public transportation
-colnames(tidydata) <- gsub("Public.transportation..excluding.taxicab...Estimate..Workers.","publictrans",colnames(tidydata))
-colnames(tidydata) <- gsub("Public.transportation..excluding.taxicab...Estimate..AGE...","publictrans",colnames(tidydata))
 colnames(tidydata) <- gsub("Public.transportation..excluding.taxicab...Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.","publictrans",colnames(tidydata))
 
 colnames(tidydata) <- gsub("\\.","",colnames(tidydata))
 
 
 # convert percentages to numeric, percentage by decimal
-tidydata[3:8] <- sapply(tidydata[3:8],function(x) as.numeric(x)/100)
-tidydata[10:15] <- sapply(tidydata[10:15],function(x) as.numeric(x)/100)
-tidydata[17:22] <- sapply(tidydata[17:22],function(x) as.numeric(x)/100)
-tidydata[24:31] <- sapply(tidydata[24:31],function(x) as.numeric(x)/100)
-tidydata[33:40] <- sapply(tidydata[33:40],function(x) as.numeric(x)/100)
-tidydata[42:49] <- sapply(tidydata[42:49],function(x) as.numeric(x)/100)
+tidydata[3:10] <- sapply(tidydata[3:10],function(x) as.numeric(x)/100)
+tidydata[12:19] <- sapply(tidydata[12:19],function(x) as.numeric(x)/100)
+tidydata[21:28] <- sapply(tidydata[21:28],function(x) as.numeric(x)/100)
 
--- server.R
+++ server.R
@@ -4,23 +4,27 @@ library(ggplot2)
 
 shinyServer(function(input,output){
 
-  output$prob <- renderText({
-    state.df <- subset(tidydata, state == input$state)
-    age <- as.numeric(input$age)
+  output$plot <- renderPlot({
+    
+  })
+  output$prob <- renderTable({
+    
+    state.df <- subset(tidydata, state == input$state)    
     income <- as.numeric(input$income)
     
-    # method of commute by age
-    totaldrovealone <- state.df$drovealone16yearsandover[1] * state.df[,age]
-    totalcarpool <- state.df$carpooled16yearsandover[1] * state.df[,age+7]
-    totalpublictrans <- state.df$publictrans16yearsandover[1] * state.df[,age+14]
-    
-    # sum of above methods
-    sum <- sum(totaldrovealone,totalcarpool,totalpublictrans)
-    
     # method of commute by income
-    totalincomelevel.drivealone <- state.df$drovealone16yearsandoverwithearnings * state.df[,income]
+    totalincomelevel.drovealone <- state.df$drovealone16yearsandoverwithearnings * state.df[,income]
     totalincomelevel.carpool <- state.df$drovealone16yearsandoverwithearnings * state.df[,income + 9]
     totalincomelevel.publictrans <- state.df$drovealone16yearsandoverwithearnings * state.df[,income + 18]
+    
+    # sum of above methods
+    sum.income <- sum(totalincomelevel.drovealone,totalincomelevel.carpool,totalincomelevel.publictrans)
+    
+    results.df <- data.frame(Method = c("Drive Alone*","Carpool","Public Transportation**"),
+                             Probability = c((totalincomelevel.drovealone/sum.income),
+                                             (totalincomelevel.carpool/sum.income),
+                                             (totalincomelevel.publictrans/sum.income)))
+    results.df
     
   })
 
