ba69e8cb69dadf1f0c140cff8dba505a8f8af480
mattsedlar <sedlarm@gmail.com>
2015-09-17 14:29:08 -0700

    Expected value of commute by age

--- server.R
+++ server.R
@@ -9,13 +9,15 @@ shinyServer(function(input,output){
     age <-as.numeric(input$age)
     income <- input$income
     
+    # method of commute by age
     totaldrovealone <- state.df$drovealone16yearsandover[1] * state.df[,age]
     totalcarpool <- state.df$carpooled16yearsandover[1] * state.df[,age+7]
     totalpublictrans <- state.df$publictrans16yearsandover[1] * state.df[,age+14]
 
     sum <- sum(totaldrovealone,totalcarpool,totalpublictrans)
     
-    
+    probA <- sum((1*(totaldrovealone/sum)) + (2 *(totalcarpool/sum)) + (3 * (totalpublictrans/sum)))
+    probA    
   })
 
 })
