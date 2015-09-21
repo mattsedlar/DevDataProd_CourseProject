52a86cc3593856d4d401915ba0be3cd67bda4693
mattsedlar <sedlarm@gmail.com>
2015-09-21 13:57:54 -0700

    Fixed mistake in calculating totals, added margins of error

--- scripts/cleaning.R
+++ scripts/cleaning.R
@@ -13,49 +13,78 @@ if(!exists("data")){
 tidydata <- data
 
 # selecting only columns necessary for calculations
-tidydata <- tidydata %>% select(Geography,
-                                Car..truck..or.van....drove.alone..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings,
-                                Car..truck..or.van....drove.alone..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....1.to..9.999.or.loss,
-                                Car..truck..or.van....drove.alone..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....10.000.to..14.999,
-                                Car..truck..or.van....drove.alone..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....15.000.to..24.999,
-                                Car..truck..or.van....drove.alone..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....25.000.to..34.999,
-                                Car..truck..or.van....drove.alone..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....35.000.to..49.999,
-                                Car..truck..or.van....drove.alone..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....50.000.to..64.999,
-                                Car..truck..or.van....drove.alone..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....65.000.to..74.999,
-                                Car..truck..or.van....drove.alone..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....75.000.or.more,
-                                Car..truck..or.van....carpooled..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings,
-                                Car..truck..or.van....carpooled..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....1.to..9.999.or.loss,
-                                Car..truck..or.van....carpooled..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....10.000.to..14.999,
-                                Car..truck..or.van....carpooled..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....15.000.to..24.999,
-                                Car..truck..or.van....carpooled..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....25.000.to..34.999,
-                                Car..truck..or.van....carpooled..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....35.000.to..49.999,
-                                Car..truck..or.van....carpooled..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....50.000.to..64.999,
-                                Car..truck..or.van....carpooled..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....65.000.to..74.999,
-                                Car..truck..or.van....carpooled..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....75.000.or.more,
-                                Public.transportation..excluding.taxicab...Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings,
-                                Public.transportation..excluding.taxicab...Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....1.to..9.999.or.loss,
-                                Public.transportation..excluding.taxicab...Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....10.000.to..14.999,
-                                Public.transportation..excluding.taxicab...Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....15.000.to..24.999,
-                                Public.transportation..excluding.taxicab...Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....25.000.to..34.999,
-                                Public.transportation..excluding.taxicab...Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....35.000.to..49.999,
-                                Public.transportation..excluding.taxicab...Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....50.000.to..64.999,
-                                Public.transportation..excluding.taxicab...Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....65.000.to..74.999,
-                                Public.transportation..excluding.taxicab...Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....75.000.or.more)
+tidydata <- tidydata %>% select(Geography, #1
+                                Car..truck..or.van....drove.alone..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings, #2
+                                Car..truck..or.van....drove.alone..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....1.to..9.999.or.loss, #3
+                                Car..truck..or.van....drove.alone..Margin.of.Error..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....1.to..9.999.or.loss, #4
+                                Car..truck..or.van....drove.alone..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....10.000.to..14.999, #5
+                                Car..truck..or.van....drove.alone..Margin.of.Error..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....10.000.to..14.999, #6
+                                Car..truck..or.van....drove.alone..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....15.000.to..24.999, #7
+                                Car..truck..or.van....drove.alone..Margin.of.Error..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....15.000.to..24.999, #8
+                                Car..truck..or.van....drove.alone..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....25.000.to..34.999, #9
+                                Car..truck..or.van....drove.alone..Margin.of.Error..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....25.000.to..34.999, #10
+                                Car..truck..or.van....drove.alone..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....35.000.to..49.999, #11
+                                Car..truck..or.van....drove.alone..Margin.of.Error..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....35.000.to..49.999, #12
+                                Car..truck..or.van....drove.alone..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....50.000.to..64.999, #13
+                                Car..truck..or.van....drove.alone..Margin.of.Error..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....50.000.to..64.999, #14
+                                Car..truck..or.van....drove.alone..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....65.000.to..74.999, #15
+                                Car..truck..or.van....drove.alone..Margin.of.Error..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....65.000.to..74.999, #16
+                                Car..truck..or.van....drove.alone..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....75.000.or.more, #17
+                                Car..truck..or.van....drove.alone..Margin.of.Error..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....75.000.or.more, #18
+                                Car..truck..or.van....carpooled..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings, #19
+                                Car..truck..or.van....carpooled..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....1.to..9.999.or.loss, #20
+                                Car..truck..or.van....carpooled..Margin.of.Error..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....1.to..9.999.or.loss, #21
+                                Car..truck..or.van....carpooled..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....10.000.to..14.999, #22
+                                Car..truck..or.van....carpooled..Margin.of.Error..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....10.000.to..14.999, #23
+                                Car..truck..or.van....carpooled..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....15.000.to..24.999, #24
+                                Car..truck..or.van....carpooled..Margin.of.Error..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....15.000.to..24.999, #25
+                                Car..truck..or.van....carpooled..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....25.000.to..34.999, #26
+                                Car..truck..or.van....carpooled..Margin.of.Error..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....25.000.to..34.999, #27
+                                Car..truck..or.van....carpooled..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....35.000.to..49.999, #28
+                                Car..truck..or.van....carpooled..Margin.of.Error..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....35.000.to..49.999, #29
+                                Car..truck..or.van....carpooled..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....50.000.to..64.999, #30
+                                Car..truck..or.van....carpooled..Margin.of.Error..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....50.000.to..64.999, #31
+                                Car..truck..or.van....carpooled..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....65.000.to..74.999, #32
+                                Car..truck..or.van....carpooled..Margin.of.Error..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....65.000.to..74.999, #33
+                                Car..truck..or.van....carpooled..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....75.000.or.more, #34
+                                Car..truck..or.van....carpooled..Margin.of.Error..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....75.000.or.more, #35
+                                Public.transportation..excluding.taxicab...Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings, #36
+                                Public.transportation..excluding.taxicab...Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....1.to..9.999.or.loss, #37
+                                Public.transportation..excluding.taxicab...Margin.of.Error..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....1.to..9.999.or.loss, #38
+                                Public.transportation..excluding.taxicab...Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....10.000.to..14.999, #39
+                                Public.transportation..excluding.taxicab...Margin.of.Error..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....10.000.to..14.999, #40
+                                Public.transportation..excluding.taxicab...Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....15.000.to..24.999, #41
+                                Public.transportation..excluding.taxicab...Margin.of.Error..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....15.000.to..24.999, #42
+                                Public.transportation..excluding.taxicab...Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....25.000.to..34.999, #43
+                                Public.transportation..excluding.taxicab...Margin.of.Error..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....25.000.to..34.999, #44
+                                Public.transportation..excluding.taxicab...Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....35.000.to..49.999, #45
+                                Public.transportation..excluding.taxicab...Margin.of.Error..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....35.000.to..49.999, #46
+                                Public.transportation..excluding.taxicab...Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....50.000.to..64.999, #47
+                                Public.transportation..excluding.taxicab...Margin.of.Error..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....50.000.to..64.999, #48
+                                Public.transportation..excluding.taxicab...Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....65.000.to..74.999, #49
+                                Public.transportation..excluding.taxicab...Margin.of.Error..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....65.000.to..74.999, #50
+                                Public.transportation..excluding.taxicab...Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....75.000.or.more, #51
+                                Public.transportation..excluding.taxicab...Margin.of.Error..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.16.years.and.over.with.earnings....75.000.or.more) #52
 
 # rename columns
 # state
 colnames(tidydata) <- gsub("Geography","state",colnames(tidydata))
 # drove alone
 colnames(tidydata) <- gsub("Car..truck..or.van....drove.alone..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.","drovealone",colnames(tidydata))
+colnames(tidydata) <- gsub("Car..truck..or.van....drove.alone..Margin.of.Error..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.","drovealonemargin",colnames(tidydata))
+
 # carpool
 colnames(tidydata) <- gsub("Car..truck..or.van....carpooled..Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.","carpooled",colnames(tidydata))
+colnames(tidydata) <- gsub("Car..truck..or.van....carpooled..Margin.of.Error..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.","carpooledmargin",colnames(tidydata))
+
 # public transportation
 colnames(tidydata) <- gsub("Public.transportation..excluding.taxicab...Estimate..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.","publictrans",colnames(tidydata))
+colnames(tidydata) <- gsub("Public.transportation..excluding.taxicab...Margin.of.Error..EARNINGS.IN.THE.PAST.12.MONTHS..IN.2014.INFLATION.ADJUSTED.DOLLARS..FOR.WORKERS...Workers.","publictransmargin",colnames(tidydata))
 
 colnames(tidydata) <- gsub("\\.","",colnames(tidydata))
 
 
 # convert percentages to numeric, percentage by decimal
-tidydata[3:10] <- sapply(tidydata[3:10],function(x) as.numeric(x)/100)
-tidydata[12:19] <- sapply(tidydata[12:19],function(x) as.numeric(x)/100)
-tidydata[21:28] <- sapply(tidydata[21:28],function(x) as.numeric(x)/100)
+tidydata[3:18] <- sapply(tidydata[3:18],function(x) as.numeric(x)/100)
+tidydata[20:35] <- sapply(tidydata[20:35],function(x) as.numeric(x)/100)
+tidydata[37:52] <- sapply(tidydata[37:52],function(x) as.numeric(x)/100)
--- server.R
+++ server.R
@@ -22,17 +22,42 @@ shinyServer(function(input,output){
     
     # method of commute by income
     totalincomelevel.drovealone <- state.df$drovealone16yearsandoverwithearnings * state.df[,income]
-    totalincomelevel.carpool <- state.df$drovealone16yearsandoverwithearnings * state.df[,income + 9]
-    totalincomelevel.publictrans <- state.df$drovealone16yearsandoverwithearnings * state.df[,income + 18]
+    totalincomelevel.carpool <- state.df$carpooled16yearsandoverwithearnings * state.df[,income + 17]
+    totalincomelevel.publictrans <- state.df$publictrans16yearsandoverwithearnings * state.df[,income + 34]
     
     # sum of above methods
     sum.income <- sum(totalincomelevel.drovealone,totalincomelevel.carpool,totalincomelevel.publictrans)
     
-    results.df <- data.frame(Method = c("Drive Alone*","Carpool","Public Transportation**"),
+    # upper and lower margins
+    uppermargin.drovealone <- state.df$drovealone16yearsandoverwithearnings * (state.df[,income] + state.df[,income + 1])
+    lowermargin.drovealone <- state.df$drovealone16yearsandoverwithearnings * (state.df[,income] - state.df[,income + 1])
+
+    uppermargin.carpooled <- state.df$carpooled16yearsandoverwithearnings * (state.df[,income + 17] + state.df[,income + 18])
+    lowermargin.carpooled <- state.df$carpooled16yearsandoverwithearnings * (state.df[,income + 17] - state.df[,income + 18])
+    
+    uppermargin.publictrans <- state.df$publictrans16yearsandoverwithearnings * (state.df[,income + 34] + state.df[,income + 35])
+    lowermargin.publictrans <- state.df$publictrans16yearsandoverwithearnings * (state.df[,income + 34] - state.df[,income + 35])
+    
+    # create a dataframe with method, percentage and margins for plotting
+    results.df <- data.frame(Method = as.factor(c("Drive Alone*","Carpool","Public Transportation**")),
                              Percentage = c((totalincomelevel.drovealone/sum.income),
                                             (totalincomelevel.carpool/sum.income),
-                                            (totalincomelevel.publictrans/sum.income)))
-    p <- ggplot(results.df) + geom_bar(aes(Method,Percentage, fill=Percentage), stat="identity")
+                                            (totalincomelevel.publictrans/sum.income)),
+                             `Upper Margin` = c((uppermargin.drovealone/sum.income),
+                                                (uppermargin.carpooled/sum.income),
+                                                (uppermargin.publictrans/sum.income)),
+                             `Lower Margin` = c((lowermargin.drovealone/sum.income),
+                                                (lowermargin.carpooled/sum.income),
+                                                (lowermargin.publictrans/sum.income)))
+    # refactor levels
+    results.df$Method <- factor(results.df$Method, levels=c("Drive Alone*",
+                                                            "Carpool",
+                                                            "Public Transportation**"))
+    # plot
+    p <- ggplot(results.df, aes(fill=Percentage, y=Percentage, x=Method)) + 
+      geom_bar(position="dodge", stat="identity") +
+      geom_errorbar(aes(ymin=Lower.Margin,ymax=Upper.Margin)) +
+      ggtitle("Methods of Commuting with Margin of Error as Error Bars")
     print(p)
   }, height=350)  
 })
